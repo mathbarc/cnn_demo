fixing dataset class label handling, adding clipping to gradient

obj_detection/model.py
@@ -43,6 +43,9 @@ class YoloOutput(torch.nn.Module):
         classes = torch.softmax(grid[:,:,5:],dim=2)
 
         final_boxes = torch.cat((x,y,w,h,obj,classes), dim=2)
+        
+        if torch.isnan(final_boxes).any():
+            ...
 
         return final_boxes
     
@@ -150,24 +153,26 @@ def calc_batch_loss(detections:torch.Tensor, annotations, obj_gain, no_obj_gain)
             contains_obj[cellY, cellX, best_iou_id, 1] = iou[best_iou_id]
 
         zero = torch.zeros([1], device=detections.device)
+        one = torch.ones([1], device=detections.device)
 
         for i in range(detections.shape[0]):
             for j in range(detections.shape[1]):
                 for k in range(detections.shape[2]):
                     item = contains_obj[i,j,k]
-                    ann_id = item[0].int().cpu().item()
+                    ann_id = int(item[0].cpu().item())
                     best_iou = item[1]
 
                     if ann_id >= 0:
                         batch_position_loss += torch.nn.functional.mse_loss(obj_boxes[i,j,k,0:2],ann_boxes[ann_id,0:2],reduction="sum")
-                        batch_scale_loss += torch.nn.functional.mse_loss(torch.sqrt(obj_boxes[i,j,k,2:4]),torch.sqrt(ann_boxes[ann_id,2:4]),reduction="sum")
-                        batch_obj_detection_loss += obj_gain * torch.nn.functional.mse_loss(detections[i,j,k,4], best_iou)
+                        batch_position_loss += torch.nn.functional.mse_loss(torch.sqrt(obj_boxes[i,j,k,2:4]),torch.sqrt(ann_boxes[ann_id,2:4]),reduction="sum")
+                        batch_obj_detection_loss += obj_gain * torch.nn.functional.mse_loss(detections[i,j,k,4], one)
                         batch_classification_loss += torch.nn.functional.cross_entropy(detections[i,j,k,5:], ann_classes[ann_id])
                     else:
                         batch_obj_detection_loss += no_obj_gain * torch.nn.functional.mse_loss(detections[i,j,k,4].view([1]), zero)
 
-                    
-    return batch_position_loss, batch_scale_loss, batch_classification_loss, batch_obj_detection_loss
+    if torch.isnan(batch_position_loss) or torch.isnan(batch_classification_loss) or torch.isnan(batch_obj_detection_loss):
+        ...
+    return batch_position_loss, batch_classification_loss, batch_obj_detection_loss
 
 
 def calc_obj_detection_loss(
@@ -178,13 +183,14 @@ def calc_obj_detection_loss(
     obj_gain: float = 5.0,
     no_obj_gain: float = .5,
     parallel: bool = False
-) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]:
     n_batches = detections.size()[0]
 
     position_loss = 0
     scale_loss = 0
     classification_loss = 0
     obj_detection_loss = 0
+    
 
     
 
@@ -200,25 +206,24 @@ def calc_obj_detection_loss(
 
         for batch_process in batch_processing:
             
-            batch_position_loss, batch_scale_loss, batch_classification_loss, batch_obj_detection_loss = batch_process.result()
+            batch_position_loss, batch_classification_loss, batch_obj_detection_loss = batch_process.result()
 
             position_loss += batch_position_loss
-            scale_loss += batch_scale_loss
             classification_loss += batch_classification_loss
             obj_detection_loss += batch_obj_detection_loss
     else:
 
         for batch_id in range(n_batches):
-            batch_position_loss, batch_scale_loss, batch_classification_loss, batch_obj_detection_loss = calc_batch_loss(detections[batch_id], annotations[batch_id], obj_gain, no_obj_gain)
+            if len(annotations[batch_id]["labels"]) == 0:
+                continue
+            batch_position_loss, batch_classification_loss, batch_obj_detection_loss = calc_batch_loss(detections[batch_id], annotations[batch_id], obj_gain, no_obj_gain)
             
             position_loss += batch_position_loss
-            scale_loss += batch_scale_loss
             classification_loss += batch_classification_loss
             obj_detection_loss += batch_obj_detection_loss
 
     return (
         (coordinates_gain) * position_loss,
-        (coordinates_gain) * scale_loss,
         obj_detection_loss,
         (classification_gain) * classification_loss,
     )

obj_detection/train.py
@@ -58,10 +58,11 @@ def train(dataloader : data_loader.ObjDetectionDataLoader,
         cnn : model.YoloV2, 
         lr : float = 0.001,
         epochs : int = 1000, 
+        gradient_clip: float = 0.5,
         device=torch.device("cuda" if torch.cuda.is_available() else "cpu"),
         coordinates_loss_gain: float = 1.,
         classification_loss_gain: float = 1.,
-        obj_loss_gain: float = 5.,
+        obj_loss_gain: float = 1.,
         no_obj_loss_gain: float = 1.,
         ):
 
@@ -73,7 +74,7 @@ def train(dataloader : data_loader.ObjDetectionDataLoader,
 
     best_map = 0
 
-    mlflow.set_tracking_uri("http://mlflow.cluster.local")
+    # mlflow.set_tracking_uri("http://mlflow.cluster.local")
     experiment = mlflow.get_experiment_by_name("Object Detection")
     if experiment is None:
         experiment_id = mlflow.create_experiment("Object Detection")
@@ -88,6 +89,7 @@ def train(dataloader : data_loader.ObjDetectionDataLoader,
         "epoch": epochs,
         "coordinates_loss_gain": coordinates_loss_gain,
         "no_obj_loss_gain": no_obj_loss_gain,
+        "gradient_clip": gradient_clip
     }
     
     mlflow.log_params(training_params)
@@ -100,18 +102,19 @@ def train(dataloader : data_loader.ObjDetectionDataLoader,
     epoch_obj_detection_loss = 0
     epoch_classification_loss = 0
 
+    batch_counter = 0
     for i in range(epochs):
 
         cnn.train()
 
-        for imgs, anns in dataloader:
-
+        for imgs, anns in tqdm(dataloader):
+            
             imgs = imgs.to(device)
 
             output = cnn(imgs)
 
             (
-                position_loss, scale_loss, obj_detection_loss, classification_loss
+                position_loss, obj_detection_loss, classification_loss
             ) = model.calc_obj_detection_loss(
                 output,
                 anns,
@@ -122,36 +125,43 @@ def train(dataloader : data_loader.ObjDetectionDataLoader,
                 parallel=False
             )
 
-            total_loss = position_loss + scale_loss + obj_detection_loss + classification_loss
+            total_loss = position_loss + obj_detection_loss + classification_loss
 
             epoch_total_loss += total_loss.item()
             epoch_position_loss += position_loss.item()
-            epoch_scale_loss += scale_loss.item()
             epoch_obj_detection_loss += obj_detection_loss.item()
             epoch_classification_loss += classification_loss.item()
 
             total_loss.backward()
+            torch.nn.utils.clip_grad_norm_(cnn.parameters(), gradient_clip)
             optimizer.step()
 
-        scheduler.step()
-        metrics = {
-            "total_loss": epoch_total_loss,
-            "position_loss": epoch_position_loss,
-            "scale_loss": epoch_scale_loss,
-            "class_loss": epoch_classification_loss,
-            "object_presence_loss": epoch_obj_detection_loss,
-            "lr": scheduler.get_last_lr()[0],
-        }
+            batch_counter+=1
+            metrics = {
+                "total_loss": total_loss.item(),
+                "position_loss": position_loss.item(),
+                "class_loss": classification_loss.item(),
+                "object_presence_loss": obj_detection_loss.item()
+            }
+            
+            mlflow.log_metrics(metrics, batch_counter)
 
+        scheduler.step()
         cnn.eval()
+        
+        metrics = {
+                "epoch_total_loss": epoch_total_loss,
+                "epoch_position_loss": epoch_position_loss,
+                "epoch_class_loss": epoch_classification_loss,
+                "epoch_object_presence_loss": epoch_obj_detection_loss,
+                "lr": scheduler.get_last_lr()[0],
+            }
 
         performance_metrics = calculate_metrics(
             cnn, validation_dataset, device
         )
-        metrics["valid_map"] = performance_metrics
-
-
-        mlflow.log_metrics(metrics, i)
+        metrics["epoch_valid_map"] = performance_metrics
+        mlflow.log_metrics(metrics, batch_counter)
 
         if best_map < performance_metrics:
             best_map = performance_metrics
@@ -172,13 +182,17 @@ if __name__ == "__main__":
     else:
         device = torch.device("cpu")
 
-    dataset = data_loader.CocoDataset("/data/hd1/Dataset/Coco/images","/data/hd1/Dataset/Coco/annotations/instances_train2017.json")
-    validation_dataset = data_loader.CocoDataset("/data/hd1/Dataset/Coco/images_valid","/data/hd1/Dataset/Coco/annotations/instances_val2017.json")
-    dataloader = data_loader.ObjDetectionDataLoader(dataset, 8, 368, 512)
+    dataset = data_loader.CocoDataset("/data/hd1/Dataset/Coco/train2017","/data/hd1/Dataset/Coco/annotations/instances_train2017.json")
+    validation_dataset = data_loader.CocoDataset("/data/hd1/Dataset/Coco/val2017","/data/hd1/Dataset/Coco/annotations/instances_val2017.json")
+    
+    # dataset = data_loader.CocoDataset("/data/hd1/Dataset/leafs/images","/data/hd1/Dataset/leafs/annotations/instances_Train.json")
+    # validation_dataset = data_loader.CocoDataset("/data/hd1/Dataset/leafs/images","/data/hd1/Dataset/leafs/annotations/instances_Test.json")
+    
+    dataloader = data_loader.ObjDetectionDataLoader(dataset, 32, 368, 512)
 
     cnn = model.YoloV2(3, dataset.get_categories_count(), [[10,14],[23,27],[37,58],[81,82],[135,169],[344,319]])
 
-    train(dataloader, validation_dataset, cnn)
+    train(dataloader, validation_dataset, cnn, 1e-3)
 
 
 