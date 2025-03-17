6b100ab1140bee91e7ba4a6e443ac4e4b9d307b4
Matheus Barcelos de Oliveira <matheus.oliveira@syngenta.com>
2025-03-17 00:20:33 -0300

    cleaning training script

--- train_object_detection.py
+++ train_object_detection.py
@@ -1,11 +1,6 @@
 import torch
 from obj_detection import train, model, data_loader
 from obj_detection.lr_functions import (
-    ObjDetectionCosineAnnealingLR,
-    ObjDetectionExponentialDecayLR,
-    ObjDetectionCosineDecayLR,
-    ObjDetectionLogisticDecayLR,
-    ObjDetectionRampUpLR,
     YoloObjDetectionRampUpLR,
 )
 
@@ -24,35 +19,19 @@ if __name__ == "__main__":
         "/data/ssd1/Datasets/Coco/annotations/instances_val2017.json",
     )
 
-    # dataset = data_loader.CocoDataset("/data/hd1/Dataset/leafs/images","/data/hd1/Dataset/leafs/annotations/instances_Train.json")
-    # validation_dataset = data_loader.CocoDataset("/data/hd1/Dataset/leafs/images","/data/hd1/Dataset/leafs/annotations/instances_Test.json")
-
     dataloader = data_loader.ObjDetectionDataLoader(dataset, 32, 368, 512)
 
     cnn = model.YoloV2(3, dataset.get_categories_count(), dataset.compute_anchors(4))
-    # cnn = model.YoloV2(3, dataset.get_categories_count(), [[10,14],[23,27],[37,58],[81,82],[135,169],[344,319]])
-    # cnn = model.YoloV2(3, dataset.get_categories_count(), [[0.57273, 0.677385], [1.87446, 2.06253], [3.33843, 5.47434], [7.88282, 3.52778], [9.77052, 9.16828]])
 
     lr = 1e-3
     lr_rampup_period = 1000
     epochs = 8
     obj_loss_gain = 1.0
-    no_obj_loss_gain = 1.0
-    classification_loss_gain = 0.5
-    coordinates_loss_gain = 0.05
+    no_obj_loss_gain = 0.5
+    classification_loss_gain = 1
+    coordinates_loss_gain = 1
 
     optimizer = torch.optim.SGD(cnn.parameters(), lr, momentum=9e-1, weight_decay=5e-4)
-    # optimizer = torch.optim.Adam(cnn.parameters(), lr, weight_decay=5e-4)
-
-    # scheduler = ObjDetectionCosineDecayLR(optimizer, lr, 1e-8, (epochs*len(dataloader)), lr_rampup_period)
-    # scheduler = ObjDetectionExponentialDecayLR(optimizer, lr, 1e-8, (epochs*len(dataloader)), lr_rampup_period)
-
-    # scheduler = ObjDetectionLogisticDecayLR(
-    #     optimizer, lr, 1e-8, (epochs * len(dataloader)), lr_rampup_period
-    # )
-
-    # scheduler = ObjDetectionCosineAnnealingLR(optimizer, lr, 1e-8, lr_rampup_period, 1000, 2, 4)
-    # scheduler = ObjDetectionRampUpLR(optimizer, lr, lr_rampup_period)
     scheduler = YoloObjDetectionRampUpLR(
         optimizer,
         {
@@ -62,7 +41,7 @@ if __name__ == "__main__":
             4 * len(dataloader): 1e-6,
         },
         1e-2,
-        200,
+        lr_rampup_period,
         1e-8,
     )
 
@@ -81,5 +60,3 @@ if __name__ == "__main__":
         lr_rampup_period,
         2.0,
     )
-
-    ...
